be036b0bfd8cd5fb8bbd1fca54399f1dff6a3758
Jamie Bertram <jamie@novoideation.ca>
2017-11-11 13:39:55 -0700

    Get game working

--- MusicSolitaireDemo/AppDelegate.fs
+++ MusicSolitaireDemo/AppDelegate.fs
@@ -5,6 +5,11 @@ open System
 open UIKit
 open Foundation
 
+type Game() as game =
+    inherit Microsoft.Xna.Framework.Game() 
+    let manager = new Microsoft.Xna.Framework.GraphicsDeviceManager(game)
+    override __.Draw _ = game.GraphicsDevice.Clear Microsoft.Xna.Framework.Color.CornflowerBlue
+
 [<Register ("AppDelegate")>]
 type AppDelegate () =
     inherit UIApplicationDelegate ()
@@ -13,4 +18,6 @@ type AppDelegate () =
 
     // This method is invoked when the application is ready to run.
     override this.FinishedLaunching (app, options) =
+        let game = new Game()
+        game.Run()
         true
