13385e9c1e33f2c698af923805d39bed7004134f
Jamie Bertram <jamie@novoideation.ca>
2017-11-11 13:23:08 -0700

    Initial commit

---
+++ MusicSolitaireDemo.Core/Library.fs
@@ -0,0 +1,5 @@
+namespace MusicSolitaireDemo.Core
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ MusicSolitaireDemo.Tests/Test.fs
@@ -0,0 +1,10 @@
+﻿namespace MusicSolitaireDemo.Tests
+open System
+open NUnit.Framework
+
+[<TestFixture>]
+type Test() = 
+
+    [<Test>]
+    member x.TestCase() =
+        Assert.IsTrue(true)
