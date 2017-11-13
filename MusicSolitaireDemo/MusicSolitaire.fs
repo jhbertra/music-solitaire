bd1e0339b0af4fa6a085aa30ace4c1c37e80d9ff
Jamie Bertram <jamie@novoideation.ca>
2017-11-12 22:54:15 -0700

    Implement BeginMove and CancelMove

--- MusicSolitaireDemo/MusicSolitaire.fs
+++ MusicSolitaireDemo/MusicSolitaire.fs
@@ -76,9 +76,6 @@ let shuffleArr (rng: System.Random) arr =
 
 
 
-
-
-
 // ***************************************** //
 //                  GAME                     //
 // ***************************************** //
@@ -102,33 +99,41 @@ type Face =
     | Fa
     | So
     | La
-    | Ti
+    | Te
     | Do8
     | IV
     | V
     | I
-let faces = [ KeySignature; Do; Re; Me; Fa; So; La; Ti; Do8; IV; V; I ]
+let faces = [ KeySignature; Do; Re; Me; Fa; So; La; Te; Do8; IV; V; I ]
 
 type Card = Suit * Face
 
-type TableauNumber =
-    | One
-    | Two
-    | Three
-    | Four
-    | Five
-    | Six
-    | Seven
+type Pile =
+    | Stock
+    | Talon
+    | Tableau1
+    | Tableau2
+    | Tableau3
+    | Tableau4
+    | Tableau5
+    | Tableau6
+    | Tableau7
+    | HeartsFoundation
+    | SpadesFoundation
+    | DiamondsFoundation
+    | ClubsFoundation
 
 type DealPhaseModel = {
     deck : Card list
-    tableauDealMoves : (TableauNumber*bool) list
+    tableauDealMoves : (int*bool) list
     }
 
 type GamePhase =
     | DealPhase of DealPhaseModel
     | PlayingPhase
     | DonePhase
+
+type MovingModel = Pile * Card list
 
 type Model = {
     phase : GamePhase
@@ -145,6 +150,7 @@ type Model = {
     spadesFoundation : Card list
     diamondsFoundation : Card list
     clubsFoundation : Card list
+    moving : MovingModel option
     }
 
 
@@ -167,6 +173,7 @@ let initModel rng = {
     spadesFoundation = []
     diamondsFoundation = []
     clubsFoundation = []
+    moving = None
     phase = DealPhase {
         deck = 
             [for suit in suits do for face in faces do yield suit,face]
@@ -174,10 +181,8 @@ let initModel rng = {
             |> shuffleArr rng
             |> List.ofArray
         tableauDealMoves = 
-            let numberOrder = [One;Two;Three;Four;Five;Six;Seven]
-            numberOrder
-            |> List.mapi (fun i _ -> (List.skip i numberOrder) |> List.mapi (fun j num -> num,j=0))
-            |> List.collect (fun pass -> pass)
+            let numberOrder = [1..7]
+            numberOrder |> List.collect (fun i -> (List.skip (i-1) numberOrder) |> List.mapi (fun j num -> num,j=0))
         }
     }
 
@@ -190,6 +195,8 @@ let initModel rng = {
 type Msg =
     | DealCards
     | PopStock
+    | BeginMove of Pile * int
+    | CancelMove
 
 
 
@@ -203,6 +210,8 @@ let updateTableau tableau faceUp card =
 let update msg model =
     match model.phase with
 
+    // Dealing
+
     | DealPhase dealModel -> 
         match msg with
 
@@ -213,17 +222,22 @@ let update msg model =
                 match dealModel.tableauDealMoves with
                 | move :: remainingMoves -> 
                     match move with
-                    | One,_ -> { model with tableau1 = [card]; phase = DealPhase { deck = remainingDeck; tableauDealMoves = remainingMoves } }
-                    | Two,faceUp -> { model with tableau2 = updateTableau model.tableau2 faceUp card; phase = DealPhase { deck = remainingDeck; tableauDealMoves = remainingMoves } }
-                    | Three,faceUp -> { model with tableau3 = updateTableau model.tableau3 faceUp card; phase = DealPhase { deck = remainingDeck; tableauDealMoves = remainingMoves } }
-                    | Four,faceUp -> { model with tableau4 = updateTableau model.tableau4 faceUp card; phase = DealPhase { deck = remainingDeck; tableauDealMoves = remainingMoves } }
-                    | Five,faceUp -> { model with tableau5 = updateTableau model.tableau5 faceUp card; phase = DealPhase { deck = remainingDeck; tableauDealMoves = remainingMoves } }
-                    | Six,faceUp -> { model with tableau6 = updateTableau model.tableau6 faceUp card; phase = DealPhase { deck = remainingDeck; tableauDealMoves = remainingMoves } }
-                    | Seven,faceUp -> { model with tableau7 = updateTableau model.tableau7 faceUp card; phase = DealPhase { deck = remainingDeck; tableauDealMoves = remainingMoves } }
+                    | 1,_ -> { model with tableau1 = [card]; phase = DealPhase { deck = remainingDeck; tableauDealMoves = remainingMoves } }
+                    | 2,faceUp -> { model with tableau2 = updateTableau model.tableau2 faceUp card; phase = DealPhase { deck = remainingDeck; tableauDealMoves = remainingMoves } }
+                    | 3,faceUp -> { model with tableau3 = updateTableau model.tableau3 faceUp card; phase = DealPhase { deck = remainingDeck; tableauDealMoves = remainingMoves } }
+                    | 4,faceUp -> { model with tableau4 = updateTableau model.tableau4 faceUp card; phase = DealPhase { deck = remainingDeck; tableauDealMoves = remainingMoves } }
+                    | 5,faceUp -> { model with tableau5 = updateTableau model.tableau5 faceUp card; phase = DealPhase { deck = remainingDeck; tableauDealMoves = remainingMoves } }
+                    | 6,faceUp -> { model with tableau6 = updateTableau model.tableau6 faceUp card; phase = DealPhase { deck = remainingDeck; tableauDealMoves = remainingMoves } }
+                    | 7,faceUp -> { model with tableau7 = updateTableau model.tableau7 faceUp card; phase = DealPhase { deck = remainingDeck; tableauDealMoves = remainingMoves } }
+                    | _ -> model
                 | [] -> { model with stock = card :: model.stock; phase = DealPhase { deck = remainingDeck; tableauDealMoves = [] } }
                 ,Msg DealCards
 
         | _ -> model,Term
+
+
+
+    // Playing
 
     | PlayingPhase -> 
         match msg with
@@ -237,6 +251,158 @@ let update msg model =
                     talon = head :: model.talon
                 }
                 ,Term
+
+        | BeginMove (origin, count) ->
+            match model.moving,origin,count with
+            | None,Talon,1 when List.length model.talon > 0 ->
+                { model with
+                    talon = List.skip 1 model.talon
+                    moving = Some (origin,(List.take 1 model.talon))
+                    }
+                    ,Term
+            | None,HeartsFoundation,1 when List.length model.heartsFoundation > 0 ->
+                { model with
+                    heartsFoundation = List.skip 1 model.heartsFoundation
+                    moving = Some (origin,(List.take 1 model.heartsFoundation))
+                    }
+                    ,Term
+            | None,SpadesFoundation,1 when List.length model.spadesFoundation > 0 ->
+                { model with
+                    spadesFoundation = List.skip 1 model.spadesFoundation
+                    moving = Some (origin,(List.take 1 model.spadesFoundation))
+                    }
+                    ,Term
+            | None,DiamondsFoundation,1 when List.length model.diamondsFoundation > 0 ->
+                { model with
+                    diamondsFoundation = List.skip 1 model.diamondsFoundation
+                    moving = Some (origin,(List.take 1 model.diamondsFoundation))
+                    }
+                    ,Term
+            | None,ClubsFoundation,1 when List.length model.clubsFoundation > 0 ->
+                { model with
+                    clubsFoundation = List.skip 1 model.clubsFoundation
+                    moving = Some (origin,(List.take 1 model.clubsFoundation))
+                    }
+                    ,Term
+            | None,Tableau1,n when n <= List.length model.tableau1 && n > 0 ->
+                { model with
+                    tableau1 = List.skip n model.tableau1
+                    moving = Some (origin,(List.take n model.tableau1))
+                    }
+                    ,Term
+            | None,Tableau2,n when n <= List.length (fst model.tableau2) && n > 0 ->
+                { model with
+                    tableau2 = (List.skip n (fst model.tableau2)),(snd model.tableau2)
+                    moving = Some (origin,(List.take n (fst model.tableau2)))
+                    }
+                    ,Term
+            | None,Tableau3,n when n <= List.length (fst model.tableau3) && n > 0 ->
+                { model with
+                    tableau3 = (List.skip n (fst model.tableau3)),(snd model.tableau3)
+                    moving = Some (origin,(List.take n (fst model.tableau3)))
+                    }
+                    ,Term
+            | None,Tableau4,n when n <= List.length (fst model.tableau4) && n > 0 ->
+                { model with
+                    tableau4 = (List.skip n (fst model.tableau4)),(snd model.tableau4)
+                    moving = Some (origin,(List.take n (fst model.tableau4)))
+                    }
+                    ,Term
+            | None,Tableau5,n when n <= List.length (fst model.tableau5) && n > 0 ->
+                { model with
+                    tableau5 = (List.skip n (fst model.tableau5)),(snd model.tableau5)
+                    moving = Some (origin,(List.take n (fst model.tableau5)))
+                    }
+                    ,Term
+            | None,Tableau6,n when n <= List.length (fst model.tableau6) && n > 0 ->
+                { model with
+                    tableau6 = (List.skip n (fst model.tableau6)),(snd model.tableau6)
+                    moving = Some (origin,(List.take n (fst model.tableau6)))
+                    }
+                    ,Term
+            | None,Tableau7,n when n <= List.length (fst model.tableau7) && n > 0 ->
+                { model with
+                    tableau7 = (List.skip n (fst model.tableau7)),(snd model.tableau7)
+                    moving = Some (origin,(List.take n (fst model.tableau7)))
+                    }
+                    ,Term
+            | _ -> model,Term
+
+        | CancelMove ->
+            match model.moving with
+            | Some (Talon,cards) -> 
+                { model with
+                    talon = cards @ model.talon
+                    moving = None
+                    }
+                    ,Term
+            | Some (HeartsFoundation,cards) -> 
+                { model with
+                    heartsFoundation = cards @ model.heartsFoundation
+                    moving = None
+                    }
+                    ,Term
+            | Some (SpadesFoundation,cards) -> 
+                { model with
+                    spadesFoundation = cards @ model.spadesFoundation
+                    moving = None
+                    }
+                    ,Term
+            | Some (DiamondsFoundation,cards) -> 
+                { model with
+                    diamondsFoundation = cards @ model.diamondsFoundation
+                    moving = None
+                    }
+                    ,Term
+            | Some (ClubsFoundation,cards) -> 
+                { model with
+                    clubsFoundation = cards @ model.clubsFoundation
+                    moving = None
+                    }
+                    ,Term
+            | Some (Tableau1,cards) -> 
+                { model with
+                    tableau1 = cards @ model.tableau1
+                    moving = None
+                    }
+                    ,Term
+            | Some (Tableau2,cards) -> 
+                { model with
+                    tableau2 = (cards @ (fst model.tableau2),(snd model.tableau2))
+                    moving = None
+                    }
+                    ,Term
+            | Some (Tableau3,cards) -> 
+                { model with
+                    tableau3 = (cards @ (fst model.tableau3),(snd model.tableau3))
+                    moving = None
+                    }
+                    ,Term
+            | Some (Tableau4,cards) -> 
+                { model with
+                    tableau4 = (cards @ (fst model.tableau4),(snd model.tableau4))
+                    moving = None
+                    }
+                    ,Term
+            | Some (Tableau5,cards) -> 
+                { model with
+                    tableau5 = (cards @ (fst model.tableau5),(snd model.tableau5))
+                    moving = None
+                    }
+                    ,Term
+            | Some (Tableau6,cards) -> 
+                { model with
+                    tableau6 = (cards @ (fst model.tableau6),(snd model.tableau6))
+                    moving = None
+                    }
+                    ,Term
+            | Some (Tableau7,cards) -> 
+                { model with
+                    tableau7 = (cards @ (fst model.tableau7),(snd model.tableau7))
+                    moving = None
+                    }
+                    ,Term
+            | _ -> model,Term
         
         | _ -> model,Term
 
